01acd3c55747dbe6ee7b10a39d1fa5677924bef0
Victor <joaovictorrossatto@gmail.com>
2024-11-14 15:55:44 +0000

    All content on web

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas
 
 
-
 codes = {'code1':"", 'code2':"", 'code3':"", 'code4':""}
 
 index = 1
@@ -26,65 +25,19 @@ with col2:
 
 st.write("Below you have all of the good information about my work history.")
 
-df = pandas.read_csv('data.csv')
-
-
+df = pandas.read_csv('data.csv', sep=';')
 
 col3, col4 = st.columns(2)
 with col3:
-    st.subheader("Todo App")
-    content = """A distraction-free web app to help you focus on creating and completing tasks.
-    """
-    image = "images/1.png"
-    st.write(content)
-    st.image(image)
-    expand = st.expander("Source Code")
-    expand.code(codes['code1'])
-
-    st.subheader("Portfolio website")
-    content = """A website built entirely in pytho to showcase coding projects and apps.
-    """
-    image = "images/2.png"
-    st.write(content)
-    st.image(image)
-    expand = st.expander("Source Code")
-    expand.code(codes['code2'])
-
-    st.subheader("Typing Analyzer")
-    content = """A program that monitors the computer keyboard and returns the most typed words of the session.
-    """
-    image = "images/5.png"
-    st.write(content)
-    st.image(image)
-    expand = st.expander("Source Code")
-    expand.code(codes['code4'])
+    for index, row in df[0:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(row['url'])
 
 with col4:
-    st.subheader("Portfolio website")
-    content = """A program that monitors the computer webcam and sends an email when a new object enters the view.
-    """
-    image = "images/3.png"
-    st.write(content)
-    st.image(image)
-    expand = st.expander("Source Code")
-    expand.code(codes['code3'])
-
-
-    st.subheader("Typing Analyzer")
-    content = """A program that monitors the computer keyboard and returns the most typed words of the session.
-    """
-    image = "images/6.png"
-    st.write(content)
-    st.image(image)
-    expand = st.expander("Source Code")
-    expand.code(codes['code4'])
-
-
-    st.subheader("Typing Analyzer")
-    content = """A program that monitors the computer keyboard and returns the most typed words of the session.
-    """
-    image = "images/4.png"
-    st.write(content)
-    st.image(image)
-    expand = st.expander("Source Code")
-    expand.code(codes['code4'])
+    for index, row in df[10:20].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(row['url'])
